7ef2ba9a661149c74839028d522a5827062cd4ed
Jorge <guez_95ii@hotmail.com>
2021-12-16 01:23:59 -0500

    fix: Reparar Rutas, que apunten al método correcto

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,13 @@ use Illuminate\Support\Facades\Route;
 
 
     
-    Route::post("/events", [EventController::class, 'create'])->name("events.store");
-    Route::put("/events/{event}", [EventController::class, 'index'])->name("events.update");
-    Route::delete("/events/{event}", [EventController::class, 'create'])->name("events.destroy");
+    Route::post("/events", [EventController::class, 'store'])->name("events.store");
+    
+    Route::put("/events/{event}", [EventController::class, 'update'])->name("events.update");
+    Route::put("/events/{event}/partially", [EventController::class, 'updatePartially'])->name("events.update_partially");
+
+    Route::delete("/events/{event}", [EventController::class, 'destroy'])->name("events.destroy");
+    
 
     Route::resource("/eventsDescriptions", App\Http\Controllers\Events\EventDescriptionController::class)->except(["create","edit"]);
 
